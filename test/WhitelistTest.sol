d234f8d0d3443a25cad33b986418bdbc68f98204
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2024-06-21 23:42:10 -0600

    6-21

---
+++ test/WhitelistTest.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "../../hardhat-vultisig/contracts/Whitelist.sol";
+
+
+
+contract WhiteListTest {
+    address whitelistContractAddress;
+    function setUp() external {
+        
+    }
+
+    function test_unlimitedContributions() public {
+
+    }
+
+
+}
